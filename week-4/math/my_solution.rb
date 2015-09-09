e95695c3a502d83e086caa0912caca601108eed9
Jlesse <jlesseagile@gmail.com>
2015-09-08 19:06:50 -0500

    complete 4.3.2

---
+++ week-4/math/my_solution.rb
@@ -0,0 +1,22 @@
+# Math Methods
+
+# I worked on this challenge myself.
+
+
+# Your Solution Below
+
+def add(num_1, num_2)
+ return num_1 + num_2
+end
+
+def subtract(num_1, num_2)
+ return num_1 - num_2
+end
+
+def multiply(num_1, num_2)
+ return num_1 * num_2
+end
+
+def divide(num_1, num_2)
+ return Float(num_1)/Float(num_2)
+end
